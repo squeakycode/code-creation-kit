d5f5dd61ac8e95da44fb06579c77b40a5a009744
agau <agau@a4059567-a8be-4793-8850-e88c377ca7d4>
2011-02-25 14:30:20 +0000

    increased minor empty release type, adapted templates to empty release type

---
+++ version/version.gen.h
@@ -0,0 +1,7 @@
+
+inline void printVersionInfo()
+{
+    std::cout << "code-creation-kit text compiler [0.1.0.16]" << std::endl;
+    std::cout <<  "Copyright 2011 Andreas Gau" << std::endl;
+    std::cout << std::endl;
+}
--- version/version.tpl.h
+++ version/version.tpl.h
@@ -1,7 +1,7 @@
 
 inline void printVersionInfo()
 {
-    std::cout << "[ENTRY]["cck-product name"] Text Compiler [[ENTRY]["cck-release type"] [ENTRY]["cck-major"].[ENTRY]["cck-minor"].[ENTRY]["cck-build"].[ENTRY]["cck-revision"]]" << std::endl;
+    std::cout << "[ENTRY]["cck-product name"] text compiler [[BEGIN][ENTRY]["cck-release type"] [OR][END][ENTRY]["cck-major"].[ENTRY]["cck-minor"].[ENTRY]["cck-build"].[ENTRY]["cck-revision"]]" << std::endl;
     std::cout <<  "[ENTRY]["cck-copyright"]" << std::endl;
     std::cout << std::endl;
 }
