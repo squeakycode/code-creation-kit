f33ddd944ded7472d59850e5309849eff711c676
agau <agau@a4059567-a8be-4793-8850-e88c377ca7d4>
2011-02-25 14:19:15 +0000

    added

---
+++ version/version.tpl.h
@@ -0,0 +1,7 @@
+
+inline void printVersionInfo()
+{
+    std::cout << "[ENTRY]["cck-product name"] Text Compiler [[ENTRY]["cck-release type"] [ENTRY]["cck-major"].[ENTRY]["cck-minor"].[ENTRY]["cck-build"].[ENTRY]["cck-revision"]]" << std::endl;
+    std::cout <<  "[ENTRY]["cck-copyright"]" << std::endl;
+    std::cout << std::endl;
+}
