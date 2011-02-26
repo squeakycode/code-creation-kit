784c3d1d3b1c48ba9950b31c18ab19cd7b35b201
agau <agau@a4059567-a8be-4793-8850-e88c377ca7d4>
2011-02-26 21:52:16 +0000

    fixes problems when run as admin under vista and windows 7

--- examples/create_syntax_highlighter/create_and_install.cmd
+++ examples/create_syntax_highlighter/create_and_install.cmd
@@ -3,10 +3,16 @@ rem Creates and installs language definitions for PSPad editor
 rem The language definition files need to be activated
 rem Consult PSPad help on how to do this
 
+rem This batch may need to be run as administrator when run under Vista or Windows 7
+rem because it adds files to the PSPad Syntax subfolder in the Program Files Folder.
+
 if not exist "%ProgramFiles%\PSPad editor" (
     echo PSPad editor installation not found
     goto :exit_failure
 )
+
+rem change path back to where the cmd file resides when run as admin
+if /I "%CD%" EQU "%SystemRoot%\System32" cd /d "%~dp0"
 
 "%CCK_ROOT%\bin\ccktc.exe" pspad.tccmd
 if %errorlevel% neq 0 goto :exit_failure
