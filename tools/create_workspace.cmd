c349e38482efd1c94e31eb1ddd5a49ea2523422f
agau <agau@a4059567-a8be-4793-8850-e88c377ca7d4>
2011-02-26 20:37:00 +0000

    fixed use ide type parameter correctly

--- tools/create_workspace.cmd
+++ tools/create_workspace.cmd
@@ -18,8 +18,8 @@ rem   along with the code-creation-kit. If not, see <http://www.gnu.org/licenses
 
 set IDE_TYPE=%~1
 
-if not defined IDE_TYPE if exist "%ProgramFiles%\Mircrosoft Visual Studio 9.0" set IDE_TYPE=vc9
-if not defined IDE_TYPE if exist "%ProgramFiles%\Mircrosoft Visual Studio 8.0" set IDE_TYPE=vc8
+if not defined IDE_TYPE if exist "%ProgramFiles%\Microsoft Visual Studio 9.0" set IDE_TYPE=vc9
+if not defined IDE_TYPE if exist "%ProgramFiles%\Microsoft Visual Studio 8" set IDE_TYPE=vc8
 
 call create_test_mpcs.cmd implementation
 if %errorlevel% neq 0 goto :exit_failure
@@ -27,7 +27,7 @@ call create_project_mpc.cmd ..\implementation implementation %CCK_ROOT%
 if %errorlevel% neq 0 goto :exit_failure
 
 pushd ..
-%MPC_ROOT%\mwc.pl -type vc9 -include mpc -features boost=1 -static cck.mwc
+%MPC_ROOT%\mwc.pl -type %IDE_TYPE% -include mpc -features boost=1 -static cck.mwc
 if %errorlevel% neq 0 popd & goto :exit_failure
 popd
 
