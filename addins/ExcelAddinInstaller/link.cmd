21cd13fd0efdd7ef13be5b4ee2ed6bbcee9da91b
agau <agau@a4059567-a8be-4793-8850-e88c377ca7d4>
2011-02-26 14:43:07 +0000

    added

---
+++ addins/ExcelAddinInstaller/link.cmd
@@ -0,0 +1,22 @@
+@echo off
+rem   Copyright (C) 2011, Andreas Gau
+rem
+rem   This file is part of the code-creation-kit.
+rem
+rem   The code-creation-kit is free software: you can redistribute it and/or modify
+rem   it under the terms of the GNU General Public License as published by
+rem   the Free Software Foundation, either version 2 of the License, or
+rem   (at your option) any later version.
+rem
+rem   The code-creation-kit is distributed in the hope that it will be useful,
+rem   but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem   GNU General Public License for more details.
+rem
+rem   You should have received a copy of the GNU General Public License
+rem   along with the code-creation-kit. If not, see <http://www.gnu.org/licenses/>.
+@echo on
+
+call "%ProgramFiles%\Microsoft Visual Studio 8\Common7\Tools\vsvars32.bat"
+LINK.EXE /EDIT /SUBSYSTEM:CONSOLE ExcelAddinInstaller.exe
+pause
