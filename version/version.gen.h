6f95ce24173d78d7d5321deaaa4a49e964715b06
agau <agau@a4059567-a8be-4793-8850-e88c377ca7d4>
2011-02-26 14:18:52 +0000

    changed to dummy version

--- version/version.gen.h
+++ version/version.gen.h
@@ -1,7 +1,7 @@
 
 inline void printVersionInfo()
 {
-    std::cout << "code-creation-kit text compiler [0.1.0.16]" << std::endl;
+    std::cout << "code-creation-kit text compiler [Test 0.0.0.0]" << std::endl;
     std::cout <<  "Copyright 2011 Andreas Gau" << std::endl;
     std::cout << std::endl;
 }
