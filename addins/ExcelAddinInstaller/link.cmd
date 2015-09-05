26991b0be99a385d97462aea2f83658d532f7b5a
agau <agau@noreply.none>
2015-09-05 17:42:01 +0200

    updated copyright year 2014->2015

--- addins/ExcelAddinInstaller/link.cmd
+++ addins/ExcelAddinInstaller/link.cmd
@@ -1,5 +1,5 @@
 @echo off
-rem   Copyright (C) 2011, Andreas Gau
+rem   Copyright (C) 2011-2015, Andreas Gau
 rem
 rem   This file is part of the code-creation-kit.
 rem
--- version/version.gen.h
+++ version/version.gen.h
@@ -2,6 +2,6 @@
 inline void printVersionInfo()
 {
     std::cout << "code-creation-kit text compiler [Test 0.0.0.0]" << std::endl;
-    std::cout <<  "Copyright 2011 Andreas Gau" << std::endl;
+    std::cout <<  "Copyright 2011-2015 Andreas Gau" << std::endl;
     std::cout << std::endl;
 }
