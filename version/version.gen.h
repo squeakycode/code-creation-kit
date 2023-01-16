dee282a186bdf784588226f9067a67047a4dcaca
Andreas Gau <andreas.gau@gmx.org>
2023-01-16 06:06:15 +0100

    updated copyright year

--- version/version.gen.h
+++ version/version.gen.h
@@ -2,6 +2,6 @@
 inline void printVersionInfo()
 {
     std::cout << "code-creation-kit text compiler v0.6" << std::endl;
-    std::cout <<  "Copyright 2011-2019 Andreas Gau" << std::endl;
+    std::cout <<  "Copyright 2011-2023 Andreas Gau" << std::endl;
     std::cout << std::endl;
 }
